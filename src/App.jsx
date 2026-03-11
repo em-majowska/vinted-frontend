9db3659f425f9b1294af51dd8fe164c9144a08f6
Ewa Majowska <e.majowska@etik.com>
2026-03-11 17:18:23 +0400

    Add routes

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Offer from "./pages/Offer";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/offer" element={<Offer />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Offer.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Offer = () => {
+  return (
+    <div>
+      <Link to="/">go to Home</Link>
+    </div>
+  );
+};
+
+export default Offer;
